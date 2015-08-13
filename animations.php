3983550280758f604f9ec467ae39f4de6fc519a4
stevefuentes <steve@swedenunlimited.com>
2015-08-13 13:14:45 -0400

    push Matt's changes to git

---
+++ animations.php
@@ -0,0 +1,72 @@
+<? include 'assets/includes/head.php'; ?>
+<body>
+	<div id="page">
+		<? include 'assets/includes/header.php'; ?>
+		<div id="content">
+			<div class="container">
+				<h1>Animations</h1>
+				<h6>Hyperlink Globe <span class="light">— A div with a class of <code>.hyperglobe</code> with 21 empty <code>span</code></span></h6>
+				<div class="hyper-animation">
+					<div class="hyperglobe lg">
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col six">
+						<h6>Ripple <span class="light">— A div with a class of <code>.ripple</code> with 1 empty <code>span</code></span></h6>
+						<div class="hyper-animation">
+							<div class="ripple">
+								<span></span>
+							</div>
+						</div>
+					</div>
+					<div class="col six">
+						<div class="hyper-animation">
+							<div class="ripple"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<? include 'assets/includes/footer.php'; ?>
+	</div>
+	<div class="hyper-grid">
+		<div class="container">
+			<div class="row">
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+				<div class="col one"></div>
+			</div>
+		</div>
+	</div>
+</body>
+<? include 'assets/includes/foot.php'; ?>
